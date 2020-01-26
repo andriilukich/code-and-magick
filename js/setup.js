f9bd7749d6837ad61f738654c1165e49b7fd1133
Andrii <andriilukich@gmail.com>
2020-01-26 17:05:12 +0200

    Draggable setup window:  - Identify avatar element, add listener to it onMouseDown  - Set event listeners on mouse Move and Up  - - Set event listener on Click with draggable flag, to seperate functionality

--- js/setup.js
+++ js/setup.js
@@ -75,7 +75,7 @@ var openPopup = function () {
 
 var closePopup = function () {
   userSetup.classList.add('hidden');
-  document.removeEventLIstener('keydown', onPopupEscPress);
+  document.removeEventListener('keydown', onPopupEscPress);
 };
 
 setupOpen.addEventListener('click', function () {
@@ -136,4 +136,62 @@ eyeSetup.addEventListener('click', function () {
 
 fireballSetup.addEventListener('click', function () {
   fireballSetup.style.backgroundColor = getRandomItem(fireballColor);
+});
+
+/* ----------------------
+
+    Draggable setup window:
+      - Identify avatar element, add listener to it onMouseDown
+      - Set event listeners on mouse Move and Up
+      - Set event listener on Click with draggable flag, to seperate functionality
+
+---------------------- */
+
+var userAvatarEl = userSetup.querySelector('.upload');
+
+userAvatarEl.addEventListener('mousedown', function (downEvt) {
+  downEvt.preventDefault();
+  var draggable = false;
+
+  var startCoords = {
+    x: downEvt.clientX,
+    y: downEvt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    draggable = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    userSetup.style.left = (userSetup.offsetLeft - shift.x) + 'px';
+    userSetup.style.top = (userSetup.offsetTop - shift.y) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (draggable) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        userAvatarEl.removeEventListener('click', onClickPreventDefault);
+      };
+
+      userAvatarEl.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
